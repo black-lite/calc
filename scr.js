3b91dace868d81d2eb35fcc9c69785b8adcc1685
black-lite <yamanowitaly@gmail.com>
2022-11-27 13:39:06 +0700

    do function check for checking input numbers, create listeners and do handlers, declaring a function calc

--- scr.js
+++ scr.js
@@ -2,4 +2,20 @@ document.addEventListener('DOMContentLoaded', function()  {
 	let first = document.querySelector('body > div > input:nth-of-type(1)');
 	let second = document.querySelector('body > div > input:nth-of-type(2)');
 	let action = document.querySelector('body > div > select');
+
+	function check() { return (first.value && second.value); }
+
+	first.addEventListener('input', () => {
+		if (check()) calc(first.value, second.value, action.value, 'body > div > input:nth-of-type(3)');
+	});
+	second.addEventListener('input', () => {
+		if (check()) calc(first.value, second.value, action.value, '#result');
+	});
+	action.addEventListener('change', () => {
+		if (check()) calc(first.value, second.value, action.value, '#result');
+	});
+
+	function calc(first, second, action, selector) {
+
+	}
 });
