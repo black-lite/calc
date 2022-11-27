97d74e2c5e67e34b15747db20655641608bcc5c2
black-lite <yamanowitaly@gmail.com>
2022-11-27 13:35:24 +0700

    find need elements

--- scr.js
+++ scr.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function()  {
-
+	let first = document.querySelector('body > div > input:nth-of-type(1)');
+	let second = document.querySelector('body > div > input:nth-of-type(2)');
+	let action = document.querySelector('body > div > select');
 });
