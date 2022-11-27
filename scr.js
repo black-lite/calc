ec86fe1def92513c748a60722dce64a4af9d4ac8
black-lite <yamanowitaly@gmail.com>
2022-11-27 14:12:21 +0700

    do function calc.

--- scr.js
+++ scr.js
@@ -5,17 +5,18 @@ document.addEventListener('DOMContentLoaded', function()  {
 
 	function check() { return (first.value && second.value); }
 
-	first.addEventListener('input', () => {
-		if (check()) calc(first.value, second.value, action.value, 'body > div > input:nth-of-type(3)');
-	});
-	second.addEventListener('input', () => {
-		if (check()) calc(first.value, second.value, action.value, '#result');
-	});
-	action.addEventListener('change', () => {
-		if (check()) calc(first.value, second.value, action.value, '#result');
-	});
+	first.addEventListener('input', () => { if (check()) calc(first.value, second.value, action.value, '#result'); });
+	second.addEventListener('input', () => { if (check()) calc(first.value, second.value, action.value, '#result'); });
+	action.addEventListener('change', () => { if (check()) calc(first.value, second.value, action.value, '#result'); });
 
 	function calc(first, second, action, selector) {
-
+		let result = document.querySelector(selector);
+		switch (action) {
+			case '+': result.textContent = Number(first) + Number(second); break;
+			case '-': result.textContent = Number(first) - Number(second); break;
+			case '*': result.textContent = Number(first) * Number(second); break;
+			case '/': Number(first) / Number(second) !== Infinity ? result.textContent = Number(first) / Number(second) : result.textContent = '---'; break;
+			default: return alert('Такие операции не обрабатываются!');
+		}
 	}
 });
